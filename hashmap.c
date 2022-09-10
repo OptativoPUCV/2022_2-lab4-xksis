4cd7148ffe1d493d2233921baa87fc7cd6e837a9
Replit user <>
2022-09-10 13:37:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -58,7 +58,19 @@ void insertMap(HashMap * map, char * key, void * value) {
 }
 
 void enlarge(HashMap * map) {
-    enlarge_called = 1; //no borrar (testing purposes)
+  enlarge_called = 1; //no borrar (testing purposes)
+  Pair **bucketaux = map->buckets;
+  size_t i;
+  map->capacity += 2;
+  map->buckets = (Pair **)calloc(map->capacity,sizeof(Pair *));
+  map->size = 0;
+  for (i = 0; i < map->capacity; i++)
+  {
+    if(bucketaux[i] != NULL)
+    {
+      insertMap(map,bucketaux[i]->key,bucketaux[i]->value);
+    }
+  }
 
 
 }
